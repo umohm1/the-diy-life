e24cb0e4c4da05578b5f0a721942265a14ec37d4
umohm1 <umohm12@gmail.com>
2017-04-03 15:17:55 -0400

    Working on adding username to signup form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :user_projects
   resources :projects
-  resources :users
+  #resources :users, only: [:show]
   root 'welcome#index'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
