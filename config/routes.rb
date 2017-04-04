a22ba78fa110bc295e60a1f9b332338ecf366f82
umohm1 <umohm12@gmail.com>
2017-04-04 12:10:14 -0400

    working on profile show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :user_projects
   resources :projects
-  #resources :users, only: [:show]
+  resources :profiles, only: [:show]
   root 'welcome#index'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
