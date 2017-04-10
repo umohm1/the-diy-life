661d2b31d5913d1e95a2cd8383898a3f6f600930
umohm1 <umohm12@gmail.com>
2017-04-10 11:43:08 -0400

    fixed update/destroy actions in projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -34,17 +34,26 @@ class ProjectsController < ApplicationController
 
   def update
     @project = Project.find(params[:id])
-      if @project.update(project_params)
+    if !current_user
+      redirect_to new_user_session_path, alert: "This project can only be edited by it's author."
+    elsif current_user != @project
+      redirect_to user_project_path, alert: "You must be the author in order to edit a project."
+    else
+      @project.update(project_params)
         redirect_to user_project_path(current_user, @project)
-      else
-      render :edit
-     end
+    end
   end
 
   def destroy
     @project = Project.find(params[:id])
+    if !current_user
+      redirect_to new_user_session_path, alert: "You cannot delete this project."
+    elsif current_user != @project
+      redirect_to user_project_path, alert: "You cannot delete this project."
+    else
     @project.destroy
     redirect_to user_projects_path
+    end
   end
 
   private
